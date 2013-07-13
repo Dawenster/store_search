50e681c15be61b40f4b5dee1a41e1ffea8ff91f0
Dawenster <dstwen@gmail.com>
2013-07-13 15:36:50 -0700

    uses custom cells for search results

---
+++ StoreSearch/SearchResultCell.h
@@ -0,0 +1,17 @@
+//
+//  SearchResultCell.h
+//  StoreSearch
+//
+//  Created by David Wen on 2013-07-13.
+//  Copyright (c) 2013 David Wen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchResultCell : UITableViewCell
+
+@property (nonatomic, weak) IBOutlet UILabel *nameLabel;
+@property (nonatomic, weak) IBOutlet UILabel *artistNameLabel;
+@property (nonatomic, weak) IBOutlet UIImageView *artworkImageView;
+
+@end
