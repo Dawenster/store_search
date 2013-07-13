96bc7101d330743fa59d38521a582c3abfe6f8f0
Dawenster <dstwen@gmail.com>
2013-07-13 14:58:59 -0700

    search bar basic implementation with local data

---
+++ StoreSearch/SearchResult.h
@@ -0,0 +1,16 @@
+//
+//  SearchResult.h
+//  StoreSearch
+//
+//  Created by David Wen on 2013-07-13.
+//  Copyright (c) 2013 David Wen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SearchResult : NSObject
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *artistName;
+
+@end
