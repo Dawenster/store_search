95b07880dd1aed0583c54a947a73f7a2f4225f2c
Dawenster <dstwen@gmail.com>
2013-07-13 20:34:27 -0700

    added pop-over

---
+++ StoreSearch/DetailViewController.m
@@ -0,0 +1,66 @@
+//
+//  DetailViewController.m
+//  StoreSearch
+//
+//  Created by David Wen on 2013-07-13.
+//  Copyright (c) 2013 David Wen. All rights reserved.
+//
+
+#import "DetailViewController.h"
+#import <QuartzCore/QuartzCore.h>
+
+@interface DetailViewController ()
+
+@property (nonatomic, weak) IBOutlet UIImageView *artworkImageView;
+@property (nonatomic, weak) IBOutlet UILabel *nameLabel;
+@property (nonatomic, weak) IBOutlet UILabel *artistNameLabel;
+@property (nonatomic, weak) IBOutlet UILabel *kindLabel;
+@property (nonatomic, weak) IBOutlet UILabel *genreLabel;
+@property (nonatomic, weak) IBOutlet UILabel *priceLabel;
+@property (nonatomic, weak) IBOutlet UIButton *storeButton;
+@property (nonatomic, weak) IBOutlet UIView *backgroundView;
+
+@end
+
+@implementation DetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UIImage *image = [[UIImage imageNamed:@"StoreButton"] stretchableImageWithLeftCapWidth:6 topCapHeight:0];
+    [self.storeButton setBackgroundImage:image forState:UIControlStateNormal];
+    
+    self.backgroundView.layer.borderColor = [UIColor whiteColor].CGColor;
+    self.backgroundView.layer.borderWidth = 3.0f;
+    self.backgroundView.layer.cornerRadius = 10.0f;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)close:(id)sender
+{
+    [self willMoveToParentViewController:nil];
+    [self.view removeFromSuperview];
+    [self removeFromParentViewController];
+}
+
+- (void)dealloc
+{
+    NSLog(@"dealloc %@", self);
+}
+
+@end
